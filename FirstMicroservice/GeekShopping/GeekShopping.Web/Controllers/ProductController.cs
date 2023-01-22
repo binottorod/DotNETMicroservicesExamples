681adab9e8ad4fa22f3ab7450820eebc29d656a4
Binotto <binotto10@gmail.com>
2023-01-22 17:53:51 -0300

    FrontEnd adjusts and created ProductController

---
+++ FirstMicroservice/GeekShopping/GeekShopping.Web/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using GeekShopping.Web.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeekShopping.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService _prouctService;
+
+        public ProductController(IProductService prouctService)
+        {
+            _prouctService = prouctService ?? throw new ArgumentNullException(nameof(prouctService));
+        }
+
+        public async Task<IActionResult> ProductIndex()    
+        {
+            var products = _prouctService.FindAllProducts();
+
+            return View(products);
+        }
+    }
+}

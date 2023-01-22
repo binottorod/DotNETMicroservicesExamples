e93a77ef1ffa44b22bc2d42918f5daf25c1c4cfa
Binotto <binotto10@gmail.com>
2023-01-22 17:08:30 -0300

    Fixed FromBody annotation missing

--- FirstMicroservice/GeekShopping/GeekShopping.ProductAPI/Controllers/ProductController.cs
+++ FirstMicroservice/GeekShopping/GeekShopping.ProductAPI/Controllers/ProductController.cs
@@ -40,7 +40,7 @@ namespace GeekShopping.ProductAPI.Controllers
         }
 
         [HttpPost]
-        public async Task<ActionResult<ProductVO>> Create(ProductVO vo)
+        public async Task<ActionResult<ProductVO>> Create([FromBody] ProductVO vo)
         {
 
             if (vo == null)
@@ -55,7 +55,7 @@ namespace GeekShopping.ProductAPI.Controllers
         }
 
         [HttpPut]
-        public async Task<ActionResult<ProductVO>> Update(ProductVO vo)
+        public async Task<ActionResult<ProductVO>> Update([FromBody] ProductVO vo)
         {
 
             if (vo == null)

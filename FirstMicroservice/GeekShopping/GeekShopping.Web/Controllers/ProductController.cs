b756c7e5cc598ff7962e366cd10b9a0506548e81
Binotto <binotto10@gmail.com>
2023-01-27 13:52:43 -0300

    Finished integration between frontend and ProductAPI

--- FirstMicroservice/GeekShopping/GeekShopping.Web/Controllers/ProductController.cs
+++ FirstMicroservice/GeekShopping/GeekShopping.Web/Controllers/ProductController.cs
@@ -31,12 +31,61 @@ namespace GeekShopping.Web.Controllers
             if (ModelState.IsValid)
             {
                 var response = await _prouctService.CreateProduct(model);
-                
+
                 if (response != null)
                 {
                     return RedirectToAction(nameof(ProductIndex));
                 }
             }
+            return View(model);
+        }
+
+        public async Task<IActionResult> ProductUpdate(int id)
+        {
+            var model = await _prouctService.FindProductById(id);
+            if (model != null)
+            {
+                return View(model);
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ProductUpdate(ProductModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var response = await _prouctService.UpdateProduct(model);
+
+                if (response != null)
+                {
+                    return RedirectToAction(nameof(ProductIndex));
+                }
+            }
+            return View(model);
+        }
+
+        public async Task<IActionResult> ProductDelete(int id)
+        {
+            var model = await _prouctService.FindProductById(id);
+            if (model != null)
+            {
+                return View(model);
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ProductDelete(ProductModel model)
+        {
+
+            var response = await _prouctService.DeleteProductById(model.Id);
+
+            if (response)
+            {
+                return RedirectToAction(nameof(ProductIndex));
+            }
+
             return View(model);
         }
     }

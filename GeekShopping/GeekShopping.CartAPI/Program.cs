323f76ad2fff8e1dd2b87722d70b438c1f68b4a8
Binotto <binotto10@gmail.com>
2023-06-08 10:51:00 -0300

    Added coupon code validation before confirm order

--- GeekShopping/GeekShopping.CartAPI/Program.cs
+++ GeekShopping/GeekShopping.CartAPI/Program.cs
@@ -20,9 +20,13 @@ IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
 builder.Services.AddSingleton(mapper);
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddScoped<ICartRepository, CartRepository>();
+builder.Services.AddScoped<ICouponRepository, CouponRepository>();
 builder.Services.AddScoped<IRabbitMQMessageSender, RabbitMQMessageSender>();
 
 builder.Services.AddControllers();
+
+builder.Services.AddHttpClient<ICouponRepository, CouponRepository>(s => s.BaseAddress =
+    new Uri(builder.Configuration["ServiceUrls:CouponAPI"]));
 
 builder.Services.AddAuthentication("Bearer")
     .AddJwtBearer("Bearer", options =>
